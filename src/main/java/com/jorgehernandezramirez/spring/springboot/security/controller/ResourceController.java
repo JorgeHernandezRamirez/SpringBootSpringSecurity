9f0bcc079501b38b1b98976d60a2f9b19031f7b6
VASS\jorge.hernandez <jorge.hernandez@vass.es>
2017-03-08 18:00:56 +0100

    Add controller with preauthorize

--- src/main/java/com/jorgehernandezramirez/spring/springboot/security/controller/ResourceController.java
+++ src/main/java/com/jorgehernandezramirez/spring/springboot/security/controller/ResourceController.java
@@ -1,5 +1,6 @@
 package com.jorgehernandezramirez.spring.springboot.security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,15 @@ public class ResourceController {
         return "Private resource";
     }
 
-    @RequestMapping("/private/admin/resource")
-    public String getAdminResouce() {
-        return "Admin resource";
+    @RequestMapping("/private/admin/resource1")
+    public String getAdminResouce1() {
+        return "Admin resource1";
     }
+
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
+    @RequestMapping("/private/admin/resource2")
+    public String getAdminResouce2() {
+        return "Admin resource2";
+    }
+
 }
